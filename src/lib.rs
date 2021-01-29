e848fa35dc30dfc45d952a8a17e4103729c78935
Max Inden <mail@max-inden.de>
2021-01-29 11:39:43 +0100

    src/lib: Rework timing calc

--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,7 @@ use std::sync::{Arc, Mutex};
 use std::task::{Context, Poll, Waker};
 use std::time::Duration;
 
-pub struct Stream {
+pub struct Connection {
     sender: UnboundedSender<Item>,
 
     receiver: UnboundedReceiver<Item>,
@@ -24,14 +24,60 @@ pub struct Stream {
     capacity: usize,
 }
 
+impl Connection {
+    /// Create a new [`Connection`] pair.
+    ///
+    /// `bandwidth` being the bandwidth in bits per second.
+    ///
+    /// `rtt` being the round trip time.
+    pub fn new(bandwidth_bits_per_second: u64, rtt: Duration) -> (Connection, Connection) {
+        let single_direction_capacity_bytes =
+            single_direction_capacity_bytes(bandwidth_bits_per_second, rtt);
+        assert!(single_direction_capacity_bytes > 0);
+        let single_direction_delay = rtt / 2;
+
+        let (a_to_b_sender, a_to_b_receiver) = unbounded();
+        let (b_to_a_sender, b_to_a_receiver) = unbounded();
+
+        let a_to_b_shared = Arc::new(Mutex::new(Default::default()));
+        let b_to_a_shared = Arc::new(Mutex::new(Default::default()));
+
+        let a = Connection {
+            sender: a_to_b_sender,
+            receiver: b_to_a_receiver,
+            next_item: None,
+
+            shared_send: a_to_b_shared.clone(),
+            shared_receive: b_to_a_shared.clone(),
+
+            delay: single_direction_delay,
+            capacity: single_direction_capacity_bytes,
+        };
+
+        let b = Connection {
+            sender: b_to_a_sender,
+            receiver: a_to_b_receiver,
+            next_item: None,
+
+            shared_send: b_to_a_shared,
+            shared_receive: a_to_b_shared,
+
+            delay: single_direction_delay,
+            capacity: single_direction_capacity_bytes,
+        };
+
+        (a, b)
+    }
+}
+
 struct Item {
     data: Vec<u8>,
     delay: Delay,
 }
 
-impl Unpin for Stream {}
+impl Unpin for Connection {}
 
-impl AsyncRead for Stream {
+impl AsyncRead for Connection {
     fn poll_read(
         mut self: Pin<&mut Self>,
         cx: &mut Context<'_>,
@@ -74,7 +120,7 @@ impl AsyncRead for Stream {
     }
 }
 
-impl AsyncWrite for Stream {
+impl AsyncWrite for Connection {
     fn poll_write(self: Pin<&mut Self>, cx: &mut Context<'_>, buf: &[u8]) -> Poll<Result<usize>> {
         let mut shared = self.shared_send.lock().unwrap();
         let n = std::cmp::min(self.capacity - shared.size, buf.len());
@@ -112,46 +158,10 @@ struct Shared {
     size: usize,
 }
 
-// TODO: Document whether delay is the delay for both directions or just a
-// single?
-/// `bandwidth` being the one way connection bandwidth in bit/s.
-pub fn new(bandwidth: u64, delay: Duration) -> (Stream, Stream) {
-    let bandwidth_delay_product: u128 = bandwidth as u128 * delay.as_millis() / 1000u128 / 8;
-    assert!(bandwidth_delay_product > 0);
-
-    let (a_to_b_sender, a_to_b_receiver) = unbounded();
-    let (b_to_a_sender, b_to_a_receiver) = unbounded();
-
-    let a_to_b_shared = Arc::new(Mutex::new(Default::default()));
-    let b_to_a_shared = Arc::new(Mutex::new(Default::default()));
-
-    let a = Stream {
-        sender: a_to_b_sender,
-        receiver: b_to_a_receiver,
-        next_item: None,
-
-        shared_send: a_to_b_shared.clone(),
-        shared_receive: b_to_a_shared.clone(),
-
-        delay,
-        // TODO: Remove hack and should this be devided in half?
-        capacity: bandwidth_delay_product as usize,
-    };
-
-    let b = Stream {
-        sender: b_to_a_sender,
-        receiver: a_to_b_receiver,
-        next_item: None,
-
-        shared_send: b_to_a_shared,
-        shared_receive: a_to_b_shared,
-
-        delay,
-        // TODO: Remove hack and should this be devided in half?
-        capacity: bandwidth_delay_product as usize,
-    };
-
-    (a, b)
+fn single_direction_capacity_bytes(bandwidth_bits_per_second: u64, rtt: Duration) -> usize {
+    let bandwidth_delay_product: u128 =
+        bandwidth_bits_per_second as u128 * rtt.as_micros() / 1_000_000u128 / 8;
+    (bandwidth_delay_product / 2) as usize
 }
 
 #[cfg(test)]
@@ -162,15 +172,16 @@ mod tests {
     use quickcheck::{Gen, QuickCheck, TestResult};
     use std::time::Instant;
 
+    #[ignore]
     #[test]
     fn timing() {
         let bandwidth = 6 * 1024 * 1024;
         let delay = Duration::from_millis(900);
-        let msg = vec![0;10 * 1024 * 1024];
+        let msg = vec![0; 10 * 1024 * 1024];
         let msg_clone = msg.clone();
         let start = Instant::now();
 
-        let (mut a, mut b) = new(bandwidth, delay);
+        let (mut a, mut b) = Connection::new(bandwidth, delay);
 
         let mut pool = futures::executor::LocalPool::new();
 
@@ -180,7 +191,7 @@ mod tests {
                     a.write_all(&msg_clone).await.unwrap();
                 }
                 .boxed()
-                    .into(),
+                .into(),
             )
             .unwrap();
 
@@ -199,24 +210,28 @@ mod tests {
             delay.as_secs_f64(),
             duration.as_secs_f64(),
             msg.len() / 1024 * 8,
-            (bandwidth as f64 * (duration.as_secs_f64() - delay.as_secs_f64())) / (msg.len() * 8) as f64
-
+            (bandwidth as f64 * (duration.as_secs_f64() - delay.as_secs_f64()))
+                / (msg.len() * 8) as f64
         );
     }
 
     #[test]
     fn quickcheck() {
-        fn prop(msg: Vec<u8>, bandwidth: u32, delay: u64) -> TestResult {
+        fn prop(msg: Vec<u8>, bandwidth: u32, rtt: u64) -> TestResult {
             let start = Instant::now();
 
             let bandwidth = bandwidth % 1024 * 1024 * 1024; // No more than 1 GiB.
-            let delay = delay % 1_000; // No more than 1 sec.
+            let rtt = Duration::from_micros(rtt % Duration::from_secs(1).as_millis() as u64); // No more than 1 second.
 
-            if bandwidth == 0 || delay == 0 || msg.is_empty() {
+            if bandwidth == 0
+                || rtt == Duration::from_micros(1)
+                || msg.is_empty()
+                || single_direction_capacity_bytes(bandwidth as u64, rtt) < 1
+            {
                 return TestResult::discard();
             }
 
-            let (mut a, mut b) = new(bandwidth as u64, Duration::from_millis(delay.into()));
+            let (mut a, mut b) = Connection::new(bandwidth as u64, rtt);
 
             let mut pool = futures::executor::LocalPool::new();
 
@@ -241,13 +256,13 @@ mod tests {
             let duration = start.elapsed();
 
             println!(
-                "bandwidth {} KiB/s, delay {}s duration {}s, msg len {} KiB, percentage {}",
+                "bandwidth {} KiB/s, rtt {}s duration {}s, msg len {} KiB, percentage {}",
                 bandwidth / 1024,
-                Duration::from_millis(delay).as_secs_f64(),
+                rtt.as_secs_f64(),
                 duration.as_secs_f64(),
                 msg.len() / 1024 * 8,
-                (bandwidth as f64 * (duration.as_secs_f64() - Duration::from_millis(delay).as_secs_f64())) / (msg.len() * 8) as f64
-
+                (bandwidth as f64 * (duration.as_secs_f64() - rtt.as_secs_f64() / 2.0))
+                    / (msg.len() * 8) as f64
             );
 
             TestResult::passed()
